03ba71094164c2492afa14472d9072c0833c5134
albertolop85 <37163101+albertolop85@users.noreply.github.com>
2018-11-14 17:49:52 -0600

    Another useless change

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Perros!";
+        return "Greetings from Spring Boot Perro!";
     }
     
 }
