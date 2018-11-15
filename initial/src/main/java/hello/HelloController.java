d549651f58063c3e12fbe88125be49f2d7b8cd04
albertolop85 <37163101+albertolop85@users.noreply.github.com>
2018-11-14 19:17:05 -0600

    Reverted back last changes

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Amigos!";
+        return "Greetings from Spring Boot!";
     }
     
 }
