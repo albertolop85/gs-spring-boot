413dd8d1c6d65efa051e18a0daff4f99809bdfb4
albertolop85 <37163101+albertolop85@users.noreply.github.com>
2018-11-14 18:18:22 -0600

    Induced change to fail test

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Perro!";
+        return "Greetings from Spring Boot Amigos!";
     }
     
 }
